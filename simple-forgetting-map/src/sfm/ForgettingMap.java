577748f2cb72264120194afc9fad8ff6ff12e956
Dave <its-dave@users.noreply.github.com>
2022-09-24 15:53:47 +0100

    initial implementation of methods

--- simple-forgetting-map/src/sfm/ForgettingMap.java
+++ simple-forgetting-map/src/sfm/ForgettingMap.java
@@ -1,17 +1,25 @@
 package sfm;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * ForgettingMap is a key-value map with a fixed maximum size
  * When the map is added to beyond its maximum size, the least-used entry is removed
  */
 public class ForgettingMap<K,V> {
+    private final int max;
+    private final Map<K,V> map;
+    private final Map<K,Integer> usageCountMap;
 
     /**
      * Creates a new ForgettingMap
      * @param maxSize the maximum number of entries which can be added before the least-used entry is removed
      */
     public ForgettingMap(int maxSize) {
-        
+        max = maxSize;
+        map = new ConcurrentHashMap<>(maxSize);
+        usageCountMap = new ConcurrentHashMap<>(maxSize);
     }
 
     /**
@@ -20,7 +28,13 @@ public class ForgettingMap<K,V> {
      * @param value
      */
     public void add(K key, V value) {
-
+        K leastUsed = getLeastUsedKey();
+        map.put(key, value);
+        usageCountMap.put(key, 0);
+        if (map.size() > max) {
+            map.remove(leastUsed);
+            usageCountMap.remove(leastUsed);
+        }
     }
 
     /**
@@ -29,6 +43,26 @@ public class ForgettingMap<K,V> {
      * @return the value for the specified key
      */
     public V find(K key) {
-        return null;
+        V value = map.get(key);
+        if (value != null) {
+            usageCountMap.put(key, usageCountMap.get(key) + 1);
+        }
+        return value;
+    }
+
+    /**
+     * @return the key which has accessed the least number of times
+     */
+    private K getLeastUsedKey() {
+        K leastUsedKey = null;
+        int lowestUsageCount = Integer.MAX_VALUE;
+        for (K key : usageCountMap.keySet()) {
+            Integer usageCount = usageCountMap.get(key);
+            if (usageCount < lowestUsageCount) {
+                leastUsedKey = key;
+                lowestUsageCount = usageCount;
+            }
+        }
+        return leastUsedKey;
     }
 }
