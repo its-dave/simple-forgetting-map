f7651d5112747c22ba56143c67c7df2edf2641d4
Dave <18191844+its-dave@users.noreply.github.com>
2022-09-25 21:13:12 +0100

    move functional test to a separate package (#3)

--- simple-forgetting-map/src/sfm/ForgettingMapTest.java
+++ simple-forgetting-map/src/sfm/ForgettingMapTest.java
@@ -14,64 +14,6 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class ForgettingMapTest {
-
-    @Test
-    public void testRequirements() {
-        ForgettingMap<String, Integer> map = new ForgettingMap<>(3);
-        final String TEST_KEY_1 = "key1";
-        final String TEST_KEY_2 = "key2";
-        final String TEST_KEY_3 = "key3";
-        final String TEST_KEY_4 = "key4";
-        final Integer TEST_VALUE_1 = 1;
-        final Integer TEST_VALUE_2 = 2;
-        final Integer TEST_VALUE_3 = 3;
-        final Integer TEST_VALUE_4 = 4;
-
-        // Add entries and check they exist
-        map.add(TEST_KEY_1, TEST_VALUE_1);
-        Assert.assertEquals(TEST_VALUE_1, map.find(TEST_KEY_1));
-        map.add(TEST_KEY_2, TEST_VALUE_2);
-        Assert.assertEquals(TEST_VALUE_1, map.find(TEST_KEY_1));
-        Assert.assertEquals(TEST_VALUE_2, map.find(TEST_KEY_2));
-        map.add(TEST_KEY_3, TEST_VALUE_3);
-        Assert.assertEquals(TEST_VALUE_1, map.find(TEST_KEY_1));
-        Assert.assertEquals(TEST_VALUE_2, map.find(TEST_KEY_2));
-        Assert.assertEquals(TEST_VALUE_3, map.find(TEST_KEY_3));
-
-        // Add another entry and check it replaces the least-used
-        map.add(TEST_KEY_4, TEST_VALUE_4);
-        Assert.assertEquals(TEST_VALUE_1, map.find(TEST_KEY_1));
-        Assert.assertEquals(TEST_VALUE_2, map.find(TEST_KEY_2));
-        Assert.assertEquals(null, map.find(TEST_KEY_3));
-        Assert.assertEquals(TEST_VALUE_4, map.find(TEST_KEY_4));
-
-        // Use a later entry so an earlier entry is now the least-used
-        map.find(TEST_KEY_4);
-        map.find(TEST_KEY_4);
-        map.find(TEST_KEY_4);
-
-        // Add another entry and check it replaces the least-used
-        map.add(TEST_KEY_3, TEST_VALUE_3);
-        Assert.assertEquals(TEST_VALUE_1, map.find(TEST_KEY_1));
-        Assert.assertEquals(null, map.find(TEST_KEY_2));
-        Assert.assertEquals(TEST_VALUE_3, map.find(TEST_KEY_3));
-        Assert.assertEquals(TEST_VALUE_4, map.find(TEST_KEY_4));
-
-        // Use a later entry so there is a tie for least-used
-        map.find(TEST_KEY_3);
-        map.find(TEST_KEY_3);
-        map.find(TEST_KEY_3);
-        map.find(TEST_KEY_3);
-        map.find(TEST_KEY_3);
-
-        // Add another entry and check it replaces the earliest least-used
-        map.add(TEST_KEY_2, TEST_VALUE_2);
-        Assert.assertEquals(null, map.find(TEST_KEY_1));
-        Assert.assertEquals(TEST_VALUE_2, map.find(TEST_KEY_2));
-        Assert.assertEquals(TEST_VALUE_3, map.find(TEST_KEY_3));
-        Assert.assertEquals(TEST_VALUE_4, map.find(TEST_KEY_4));
-    }
-
     @Test
     public void testAddMethodThreadSafety() {
         final Integer TEST_KEY_1 = -1;
@@ -122,10 +64,10 @@ public class ForgettingMapTest {
             try {
                 int mapSize = future.get().intValue();
                 Assert.assertTrue("map size was " + mapSize + ", should only be " + MAX_SIZE + " or " + String.valueOf(MAX_SIZE + 1), mapSize == MAX_SIZE || mapSize == MAX_SIZE + 1);
-            } catch(InterruptedException e) {
+            } catch (InterruptedException e) {
                 e.printStackTrace();
                 Assert.fail("unexpected InterruptedException: " + e);
-            } catch(ExecutionException e) {
+            } catch (ExecutionException e) {
                 e.printStackTrace();
                 Assert.fail("unexpected ExecutionException: " + e);
             }
@@ -235,10 +177,10 @@ public class ForgettingMapTest {
         for (int i = 0; i < testThreadCount; i++) {
             try {
                 futures.get(i).get();
-            } catch(InterruptedException e) {
+            } catch (InterruptedException e) {
                 e.printStackTrace();
                 Assert.fail("unexpected InterruptedException: " + e);
-            } catch(ExecutionException e) {
+            } catch (ExecutionException e) {
                 e.printStackTrace();
                 Assert.fail("unexpected ExecutionException: " + e);
             }
@@ -269,7 +211,7 @@ public class ForgettingMapTest {
     @Test
     public void testFindMethodIncrementsUsageCount() {
         final String TEST_KEY_1 = "key1";
-        
+
         ForgettingMap<String, String> map = new ForgettingMap<>(1);
         map.map.put(TEST_KEY_1, new Value<>("value"));
 
@@ -293,7 +235,7 @@ public class ForgettingMapTest {
         testValue2.incrementUseCount();
         map.map.put(TEST_KEY_1, testValue1);
         map.map.put(TEST_KEY_2, testValue2);
-        
+
         Assert.assertEquals(TEST_KEY_1, map.getLeastUsedKey());
     }
 }
